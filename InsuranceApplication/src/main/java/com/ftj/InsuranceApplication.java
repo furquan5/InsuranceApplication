854150929c327ae904774355d21a7d9b56439e1a
furquan5 <furquanthejustice555@gmail.com>
2024-02-26 15:47:46 +0530

    Added in main file

--- InsuranceApplication/src/main/java/com/ftj/InsuranceApplication.java
+++ InsuranceApplication/src/main/java/com/ftj/InsuranceApplication.java
@@ -3,11 +3,18 @@ package com.ftj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ftj.controller.ReportController;
+
 @SpringBootApplication
 public class InsuranceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InsuranceApplication.class, args);
+		
+		
+		//This is the first application that I made using git 
+		
+		ReportController reportController=new ReportController;
 	}
 
 }
